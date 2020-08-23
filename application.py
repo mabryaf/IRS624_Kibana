f40296077016453a1ecb7753c6b61d1c5b4b13b4
Mabry Fonseca <mabryaf@gmail.com>
2020-08-23 04:03:44 -0400

    context

--- application.py
+++ application.py
@@ -9,24 +9,24 @@ import ssl
 
 app = Flask(__name__)
 
-context = create_ssl_context()
-context.check_hostname = False
-context.verify_mode = ssl.CERT_NONE
-
-es = Elasticsearch(
-    ['tux-es1.cci.drexel.edu','tux-es2.cci.drexel.edu','tux-es3.cci.drexel.edu'],
-    http_auth=('faf42', 'ohs4aeceeziz'),
-    scheme="https",
-    port=9200,
-    ssl_context = context,
-)
-
 @app.route("/")
 def hello():
     return "Hello World!"
 
 @app.route('/search/', methods=['GET'])
 def search():
+    context = create_ssl_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+
+    es = Elasticsearch(
+        ['tux-es1.cci.drexel.edu','tux-es2.cci.drexel.edu','tux-es3.cci.drexel.edu'],
+        http_auth=('faf42', 'ohs4aeceeziz'),
+        scheme="https",
+        port=9200,
+        ssl_context = context,
+    )
+
     title = request.args.get('title', '')
     query = {
     "from" : 0, "size" : 10,
@@ -49,11 +49,23 @@ def search():
 def test():
     return {}
 
-@app.route('/searching/', methods=['GET'])
-def searching():
-    res = es.search(index="faf42_info624_201904_movies")
+# @app.route('/searching/', methods=['GET'])
+# def searching():
+#     context = create_ssl_context()
+#     context.check_hostname = False
+#     context.verify_mode = ssl.CERT_NONE
 
-    return res
+#     es = Elasticsearch(
+#         ['tux-es1.cci.drexel.edu','tux-es2.cci.drexel.edu','tux-es3.cci.drexel.edu'],
+#         http_auth=('faf42', 'ohs4aeceeziz'),
+#         scheme="https",
+#         port=9200,
+#         ssl_context = context,
+#     )
+
+#     res = es.search(index="faf42_info624_201904_movies")
+
+#     return res
 
 if __name__ == '__main__':
     app.run(debug=True)
