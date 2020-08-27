05915709a858b59f1dc3094c82e92aeeb815fac1
bvalmeo <bvalmeo@dxc.com>
2020-08-27 19:28:12 -0400

    Add ui

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -29,7 +29,15 @@
             }
         }
     });
-
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value; // Display the default slider value
+    
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() {
+      output.innerHTML = this.value;
+    }
+    
     // Closes responsive menu when a scroll trigger link is clicked
     $(".js-scroll-trigger").click(function () {
         $(".navbar-collapse").collapse("hide");
