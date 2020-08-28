eb0b0182bff35745c03e931b853e31686c9d1fdd
Mabry Fonseca <mabryaf@gmail.com>
2020-08-28 01:06:18 -0400

    updTE UPDte

--- application.py
+++ application.py
@@ -34,40 +34,8 @@ def home():
     
     return render_template("index.html",genres=genre, countries=country, langs=lang)
 
-@app.route("/results/", methods=['POST'])
+@app.route('/result/', methods=['GET','POST'])
 def result():
-    # input_genre = ['Drama', 'Western', 'Comedy', 'Horror', 'Mystery', 'Fantasy', 'Adventure', 'Romance', 'Sci-Fi', 'Crime', 'Musical', 'Thriller', 'Music', 'Biography', 'Action', 'History', 'War', 'Family', 'Animation', 'Sport']
-    # input_country= ['USA', 'Italy', 'Germany', 'Denmark', 'France', 'Belgium', 'Hungary', 'Russia', 'Mexico', 'Sweden', 'Australia', 'Japan', 'UK', 'Austria', 'Spain', 'Czechoslovakia', 'India', 'Brazil', 'Portugal', 'Turkey', 'Netherlands', 'Finland', 'Norway', 'Poland', 'Switzerland', 'Argentina', 'Romania', 'Canada', 'China', 'Yugoslavia', 'Greece', 'Egypt', 'Israel', 'Ireland', 'Philippines', 'Cuba', 'Bulgaria', 'Lebanon', 'Bolivia', 'Chile', 'Iran', 'Croatia', 'Peru', 'Taiwan', 'Senegal', 'Syria', 'Jamaica', 'Algeria', 'Ethiopia', 'Venezuela', 'Mali', 'Indonesia', 'Vietnam', 'Iceland', 'Colombia', 'Tunisia', 'Gibraltar', 'Albania', 'Latvia', 'Ukraine', 'Kazakhstan', 'Estonia', 'Slovakia', 'Bangladesh', 'Georgia', 'Singapore', 'Slovenia', 'Thailand', 'Kuwait', 'Lithuania', 'Ecuador', 'Liechtenstein', 'Pakistan', 'Serbia', 'Uruguay', 'Moldova', 'Malta', 'Nepal', 'Malaysia', 'Armenia', 'Luxembourg', 'Bhutan', 'Iraq', 'Bahamas', 'Jordan', 'Morocco', 'Kosovo', 'Azerbaijan', 'Greenland', 'Palestine', 'Honduras', 'Uganda', 'Guatemala', 'Cyprus', 'Korea', 'Nigeria', 'Mongolia', 'Kyrgyzstan', 'Brunei', 'Panama', 'Yemen', 'Myanmar', 'Paraguay', 'Cambodia', 'Montenegro']
-    # input_lang = ['English', 'Italian', 'German', 'Danish', 'French', 'Hungarian', 'Russian', 'Spanish', 'Swedish', 'None', 'Japanese', 'Czech', 'Portuguese', 'Ukrainian', 'Turkish', 'Yiddish', 'Dutch', 'Finnish', 'Norwegian', 'Polish', 'Romanian', 'Hindi', 'Slovenian', 'Mandarin', 'Arabic', 'Serbo-Croatian', 'Bengali', 'Albanian', 'Greek', 'Croatian', 'Urdu', 'Hebrew', 'Sinhalese', 'Esperanto', 'Korean', 'Persian', 'Slovak', 'Estonian', 'Bulgarian', 'Georgian', 'Cantonese', 'Latin', 'Kannada', 'Armenian', 'Bambara', 'Indonesian', 'Vietnamese', 'Irish', 'Malayalam', 'Icelandic', 'Serbian', 'Tamil', 'Saami', 'Bosnian', 'More', 'Latvian', 'Kazakh', 'Haitian', 'Tajik', 'Khmer', 'Wolof', 'Catalan', 'Mongolian', 'Thai', 'Afrikaans', 'Telugu', 'Marathi', 'Kirghiz', 'Macedonian', 'Chinese', 'Tagalog', 'Tibetan', 'Lithuanian', 'Punjabi', 'Frisian', 'Maya', 'Basque', 'Malay', 'Inuktitut', 'Kabuverdianu', 'Kurdish', 'Nepali', 'Dari', 'Dzongkha', 'Zulu', 'Luxembourgish', 'Guarani', 'Tarahumara', 'Flemish', 'Maltese', 'Kinyarwanda', 'Gujarati', 'Tigrigna', 'Filipino', 'Pushto', 'Azerbaijani', 'Swahili', 'Samoan', 'Pular', 'Lao', 'Amharic', 'Aromanian', 'Maori', 'Burmese', 'Gallegan', 'Welsh', 'Aboriginal', 'Assamese', 'Rhaetian', 'Yakut', 'Aymara', 'Neapolitan']
-    
-    # text = request.form['text']
-    # gens=request.form.getlist('gens')
-    # count = request.form.getlist('count')
-    # lange = request.form.getlist('lange')
-    # rating = request.form['rating']
-    # yeargte = request.form['yeargte']
-    # yearlte = request.form['yearlte']
-    
-    # genres = ''
-    # for genre in gens:
-    #     genres += genre +','
-
-    # countries = ''
-    # for country in count:
-    #     countries += country +','
-
-    # languages = ''
-    # for language in lange:
-    #     languages += language +','
-
-    # url_api = 'https://imdbrecommender.azurewebsites.net/search/?text=' + text+'&genre='+genres+'&country='+countries+'&language='+languages+'&avg_vote='+rating+'&yeargte='+yeargte+'&yearlte='+yearlte+'\''
-    # r = requests.get(url_api) 
-    # query_results = json.loads(r.text)['hits']
-    # return render_template("results.html", q_exact=query_results, texts=text, genres=input_genre, countries=input_country, langs=input_lang)
-    return "results"
-
-@app.route('/search/', methods=['GET','POST'])
-def search():
     input_genre = ['Drama', 'Western', 'Comedy', 'Horror', 'Mystery', 'Fantasy', 'Adventure', 'Romance', 'Sci-Fi', 'Crime', 'Musical', 'Thriller', 'Music', 'Biography', 'Action', 'History', 'War', 'Family', 'Animation', 'Sport']
     input_country= ['USA', 'Italy', 'Germany', 'Denmark', 'France', 'Belgium', 'Hungary', 'Russia', 'Mexico', 'Sweden', 'Australia', 'Japan', 'UK', 'Austria', 'Spain', 'Czechoslovakia', 'India', 'Brazil', 'Portugal', 'Turkey', 'Netherlands', 'Finland', 'Norway', 'Poland', 'Switzerland', 'Argentina', 'Romania', 'Canada', 'China', 'Yugoslavia', 'Greece', 'Egypt', 'Israel', 'Ireland', 'Philippines', 'Cuba', 'Bulgaria', 'Lebanon', 'Bolivia', 'Chile', 'Iran', 'Croatia', 'Peru', 'Taiwan', 'Senegal', 'Syria', 'Jamaica', 'Algeria', 'Ethiopia', 'Venezuela', 'Mali', 'Indonesia', 'Vietnam', 'Iceland', 'Colombia', 'Tunisia', 'Gibraltar', 'Albania', 'Latvia', 'Ukraine', 'Kazakhstan', 'Estonia', 'Slovakia', 'Bangladesh', 'Georgia', 'Singapore', 'Slovenia', 'Thailand', 'Kuwait', 'Lithuania', 'Ecuador', 'Liechtenstein', 'Pakistan', 'Serbia', 'Uruguay', 'Moldova', 'Malta', 'Nepal', 'Malaysia', 'Armenia', 'Luxembourg', 'Bhutan', 'Iraq', 'Bahamas', 'Jordan', 'Morocco', 'Kosovo', 'Azerbaijan', 'Greenland', 'Palestine', 'Honduras', 'Uganda', 'Guatemala', 'Cyprus', 'Korea', 'Nigeria', 'Mongolia', 'Kyrgyzstan', 'Brunei', 'Panama', 'Yemen', 'Myanmar', 'Paraguay', 'Cambodia', 'Montenegro']
     input_lang = ['English', 'Italian', 'German', 'Danish', 'French', 'Hungarian', 'Russian', 'Spanish', 'Swedish', 'None', 'Japanese', 'Czech', 'Portuguese', 'Ukrainian', 'Turkish', 'Yiddish', 'Dutch', 'Finnish', 'Norwegian', 'Polish', 'Romanian', 'Hindi', 'Slovenian', 'Mandarin', 'Arabic', 'Serbo-Croatian', 'Bengali', 'Albanian', 'Greek', 'Croatian', 'Urdu', 'Hebrew', 'Sinhalese', 'Esperanto', 'Korean', 'Persian', 'Slovak', 'Estonian', 'Bulgarian', 'Georgian', 'Cantonese', 'Latin', 'Kannada', 'Armenian', 'Bambara', 'Indonesian', 'Vietnamese', 'Irish', 'Malayalam', 'Icelandic', 'Serbian', 'Tamil', 'Saami', 'Bosnian', 'More', 'Latvian', 'Kazakh', 'Haitian', 'Tajik', 'Khmer', 'Wolof', 'Catalan', 'Mongolian', 'Thai', 'Afrikaans', 'Telugu', 'Marathi', 'Kirghiz', 'Macedonian', 'Chinese', 'Tagalog', 'Tibetan', 'Lithuanian', 'Punjabi', 'Frisian', 'Maya', 'Basque', 'Malay', 'Inuktitut', 'Kabuverdianu', 'Kurdish', 'Nepali', 'Dari', 'Dzongkha', 'Zulu', 'Luxembourgish', 'Guarani', 'Tarahumara', 'Flemish', 'Maltese', 'Kinyarwanda', 'Gujarati', 'Tigrigna', 'Filipino', 'Pushto', 'Azerbaijani', 'Swahili', 'Samoan', 'Pular', 'Lao', 'Amharic', 'Aromanian', 'Maori', 'Burmese', 'Gallegan', 'Welsh', 'Aboriginal', 'Assamese', 'Rhaetian', 'Yakut', 'Aymara', 'Neapolitan']
