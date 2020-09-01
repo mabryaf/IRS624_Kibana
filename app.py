af7d7243c881763bd7b238424774a767ad736110
bvalmeo <bvalmeo@dxc.com>
2020-08-31 20:55:39 -0400

    add random ui

--- app.py
+++ app.py
@@ -117,5 +117,75 @@ def result():
 
     return render_template("results.html", q_exact=res['hits'], texts=search, genres=input_genre, countries=input_country, langs=input_lang)
 
+@app.route('/random/', methods=['GET','POST'])
+def random():
+    input_genre = ['Drama', 'Western', 'Comedy', 'Horror', 'Mystery', 'Fantasy', 'Adventure', 'Romance', 'Sci-Fi', 'Crime', 'Musical', 'Thriller', 'Music', 'Biography', 'Action', 'History', 'War', 'Family', 'Animation', 'Sport']
+    input_country= ['USA', 'Italy', 'Germany', 'Denmark', 'France', 'Belgium', 'Hungary', 'Russia', 'Mexico', 'Sweden', 'Australia', 'Japan', 'UK', 'Austria', 'Spain', 'Czechoslovakia', 'India', 'Brazil', 'Portugal', 'Turkey', 'Netherlands', 'Finland', 'Norway', 'Poland', 'Switzerland', 'Argentina', 'Romania', 'Canada', 'China', 'Yugoslavia', 'Greece', 'Egypt', 'Israel', 'Ireland', 'Philippines', 'Cuba', 'Bulgaria', 'Lebanon', 'Bolivia', 'Chile', 'Iran', 'Croatia', 'Peru', 'Taiwan', 'Senegal', 'Syria', 'Jamaica', 'Algeria', 'Ethiopia', 'Venezuela', 'Mali', 'Indonesia', 'Vietnam', 'Iceland', 'Colombia', 'Tunisia', 'Gibraltar', 'Albania', 'Latvia', 'Ukraine', 'Kazakhstan', 'Estonia', 'Slovakia', 'Bangladesh', 'Georgia', 'Singapore', 'Slovenia', 'Thailand', 'Kuwait', 'Lithuania', 'Ecuador', 'Liechtenstein', 'Pakistan', 'Serbia', 'Uruguay', 'Moldova', 'Malta', 'Nepal', 'Malaysia', 'Armenia', 'Luxembourg', 'Bhutan', 'Iraq', 'Bahamas', 'Jordan', 'Morocco', 'Kosovo', 'Azerbaijan', 'Greenland', 'Palestine', 'Honduras', 'Uganda', 'Guatemala', 'Cyprus', 'Korea', 'Nigeria', 'Mongolia', 'Kyrgyzstan', 'Brunei', 'Panama', 'Yemen', 'Myanmar', 'Paraguay', 'Cambodia', 'Montenegro']
+    input_lang = ['English', 'Italian', 'German', 'Danish', 'French', 'Hungarian', 'Russian', 'Spanish', 'Swedish', 'None', 'Japanese', 'Czech', 'Portuguese', 'Ukrainian', 'Turkish', 'Yiddish', 'Dutch', 'Finnish', 'Norwegian', 'Polish', 'Romanian', 'Hindi', 'Slovenian', 'Mandarin', 'Arabic', 'Serbo-Croatian', 'Bengali', 'Albanian', 'Greek', 'Croatian', 'Urdu', 'Hebrew', 'Sinhalese', 'Esperanto', 'Korean', 'Persian', 'Slovak', 'Estonian', 'Bulgarian', 'Georgian', 'Cantonese', 'Latin', 'Kannada', 'Armenian', 'Bambara', 'Indonesian', 'Vietnamese', 'Irish', 'Malayalam', 'Icelandic', 'Serbian', 'Tamil', 'Saami', 'Bosnian', 'More', 'Latvian', 'Kazakh', 'Haitian', 'Tajik', 'Khmer', 'Wolof', 'Catalan', 'Mongolian', 'Thai', 'Afrikaans', 'Telugu', 'Marathi', 'Kirghiz', 'Macedonian', 'Chinese', 'Tagalog', 'Tibetan', 'Lithuanian', 'Punjabi', 'Frisian', 'Maya', 'Basque', 'Malay', 'Inuktitut', 'Kabuverdianu', 'Kurdish', 'Nepali', 'Dari', 'Dzongkha', 'Zulu', 'Luxembourgish', 'Guarani', 'Tarahumara', 'Flemish', 'Maltese', 'Kinyarwanda', 'Gujarati', 'Tigrigna', 'Filipino', 'Pushto', 'Azerbaijani', 'Swahili', 'Samoan', 'Pular', 'Lao', 'Amharic', 'Aromanian', 'Maori', 'Burmese', 'Gallegan', 'Welsh', 'Aboriginal', 'Assamese', 'Rhaetian', 'Yakut', 'Aymara', 'Neapolitan']
+
+    
+    genre=request.form.getlist('gens')
+    country = request.form.getlist('count')
+    language = request.form.getlist('lange')
+    avg_vote = request.form['avg_vote']
+    yeargte = request.form['yeargte']
+    yearlte = request.form['yearlte']
+    random = True
+
+    if not yearlte:
+        yearlte = 2020
+
+    query = {
+    "from" : 0, "size" : 30,
+    "query": {
+        "bool": {
+            "should": [],
+            "filter": [{
+                "bool": {
+                    "must": [   ]
+                }
+            }
+            ]
+            }
+        }
+    }
+
+    if random==True:
+        random_query =  {
+          "function_score": {
+            "query": { "match_all": {} },
+            "random_score": {}
+          }
+        }
+        query["query"]["bool"]["should"].append(random_query)
+    if yeargte:
+        year = {"range": { "year": { "gte": yeargte, "lte": yearlte}}}
+        query["query"]["bool"]["filter"].append(year)
+    if avg_vote:
+        avg_vote = {"range": { "avg_vote": { "gte": avg_vote }}}
+        query["query"]["bool"]["filter"].append(avg_vote)
+    if genre:
+        genre = {"match": {"genre": {"query": " ".join(genre)}}}
+        query["query"]["bool"]["filter"][0]["bool"]["must"].append(genre)
+    if country:
+        country = {"match": {"country": {"query": " ".join(country)}}}
+        query["query"]["bool"]["filter"][0]["bool"]["must"].append(country)
+    if language:
+        language = {"match": {"language": {"query": " ".join(language)}}}
+        query["query"]["bool"]["filter"][0]["bool"]["must"].append(language)
+
+    print(query)
+    res = es.search(index="",body=query)
+
+    try:
+        print("%d Entries:" % res['hits']['total']['value'])
+        for hit in res['hits']['hits']:
+            print(hit["_score"])
+            print("%(avg_vote)s: %(original_title)s (%(year)s)" % hit["_source"])
+    except:
+        res = es.search(index="faf42_movies1",body=query)
+
+    return render_template("results.html", q_exact=res['hits'], genres=input_genre, countries=input_country, langs=input_lang)
+
 if __name__ == '__main__':
     app.run(debug=True)
